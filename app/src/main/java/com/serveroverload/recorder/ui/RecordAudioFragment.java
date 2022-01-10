bddab518855e3aa22232c88a4ac180cf46df402e
Jaesoek <answo93@1thefull.com>
2022-01-10 17:08:10 +0900

    1차 변경 버전 - AsyncTask -> Runnable (Handler 방식) 으로 변경

--- app/src/main/java/com/serveroverload/recorder/ui/RecordAudioFragment.java
+++ app/src/main/java/com/serveroverload/recorder/ui/RecordAudioFragment.java
@@ -177,15 +177,21 @@ public class RecordAudioFragment extends Fragment {
 						if (pfa != null) {
 							if (!pfa_on) {
 								pfa_on = true;
-//									pfa.start();
-									pft = new PlayFrequencyTask();
-									pft.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+
+								// TODO jaeseok
+								pft = new PlayFrequencyTask();
+								handler.post(pft);
+
+//								pfa.start();
+//								pft = new PlayFrequencyTask();
+//								pft.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
 //									setFreqeuncy((double) (4000 + i));
 
 							} else {
 								pfa_on = false;
+								handler.removeCallbacks(pft);
 //								pfa.stop();
-								pft.cancel(true);
+//								pft.cancel(true);
 							}
 						}
 
@@ -732,7 +738,7 @@ public class RecordAudioFragment extends Fragment {
 		pfa.frequency = freq;
 		pft.frequency = freq;
 	}
-
+/* Jaeseok : 기존 AsyncTask
 	protected class PlayFrequencyTask extends AsyncTask<Void, Void, Void> {
 
 		protected static final int SINE = 0;
@@ -858,6 +864,122 @@ public class RecordAudioFragment extends Fragment {
 
 
 			return null;
+		}
+	}
+ */
+
+	private class PlayFrequencyTask implements Runnable {
+
+		protected static final int SINE = 0;
+		protected static final int SQUARE = 1;
+		protected static final int SAWTOOTH = 2;
+
+		protected int waveform;
+		protected boolean mute;
+
+		protected double frequency;
+		protected double level;
+
+		protected float duty;
+
+		private AudioTrack audioTrack;
+
+		public PlayFrequencyTask()
+		{
+			frequency = 1440.0;
+//            level = 16384.0;
+			level = 0.1;
+			waveform = SINE;
+			duty = 0.5f;
+		}
+
+		@Override
+		public void run() {
+
+			short[] buffer;
+
+			int rate =
+					AudioTrack.getNativeOutputSampleRate(AudioManager.STREAM_MUSIC);
+			int minSize =
+					AudioTrack.getMinBufferSize(rate, AudioFormat.CHANNEL_OUT_MONO,
+							AudioFormat.ENCODING_PCM_16BIT);
+
+			// Find a suitable buffer size
+			int[] sizes = {1024, 2048, 4096, 8192, 16384, 32768};
+			int size = 0;
+
+			for (int s : sizes)
+			{
+				if (s > minSize)
+				{
+					size = s;
+					break;
+				}
+			}
+
+			final double K = 2.0 * Math.PI / rate;
+
+			// Create the audio track
+			audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC, rate,
+					AudioFormat.CHANNEL_OUT_MONO,
+					AudioFormat.ENCODING_PCM_16BIT,
+					size, AudioTrack.MODE_STREAM);
+//										size, AudioTrack.MODE_STATIC);
+			// Check audioTrack
+
+			// Check state
+			int state = audioTrack.getState();
+
+			if (state != AudioTrack.STATE_INITIALIZED)
+			{
+				audioTrack.release();
+				// return null;
+				return;
+			}
+
+			audioTrack.play();
+
+			// Create the buffer
+			buffer = new short[size];
+
+			// Initialise the generator variables
+			double f = frequency;
+			double l = 0.0;
+			double q = 0.0;
+
+			double t = (duty * 2.0 * Math.PI) - Math.PI;
+
+			// Fill the current buffer
+			for (int i = 0; i < buffer.length; i++)
+			{
+				frequency++;
+				f += (frequency - f) / 4096.0;
+				l += ((mute ? 0.0 : level) * 16384.0 - l) / 4096.0;
+				q += ((q + (f * K)) < Math.PI) ? f * K :
+						(f * K) - (2.0 * Math.PI);
+
+				switch (waveform)
+				{
+					case SINE:
+						buffer[i] = (short) Math.round(Math.sin(q) * l);
+						break;
+
+					case SQUARE:
+						buffer[i] = (short) ((q > t) ? l : -l);
+						break;
+
+					case SAWTOOTH:
+						buffer[i] = (short) Math.round((q / Math.PI) * l);
+						break;
+				}
+			}
+
+			audioTrack.write(buffer, 0, buffer.length);
+
+			audioTrack.stop();
+			audioTrack.release();
+
+			if(pfa_on) handler.postDelayed(pft, 1000 * buffer.length / rate);
 		}
 	}
 
